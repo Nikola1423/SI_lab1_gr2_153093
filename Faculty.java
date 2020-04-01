8f66931d4ea8cf488b18963ad055e8c6d6b4b766
Nikola1423 <mishevskin25@gmail.com>
2020-04-01 21:50:33 +0200

    New Branch

---
+++ Faculty.java
@@ -0,0 +1,25 @@
+class Faculty {
+    List<Student> students;
+    String name;
+
+    public Faculty(List<Student> studentList, String name) {
+        this.students = studentList;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+}
